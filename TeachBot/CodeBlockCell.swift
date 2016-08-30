54f48df0d0a649cbe840900d2e3df56c332feb8f
Peiliang Guo <edwardplguo@gmail.com>
2016-08-30 14:57:39 -0400

    Add CodeBlockCell

---
+++ TeachBot/CodeBlockCell.swift
@@ -0,0 +1,24 @@
+//
+//  CodeBlockCell.swift
+//  TeachBot
+//
+//  Created by Edward Guo on 2016-08-30.
+//  Copyright © 2016 Pei Liang Guo. All rights reserved.
+//
+
+import UIKit
+
+class CodeBlockCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
