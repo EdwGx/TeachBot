0e1c437f40f760456030ca32ebe4a13af19b0961
Peiliang Guo <edwardplguo@gmail.com>
2016-08-31 11:21:22 -0400

    Refactor code in configureColor

--- TeachBot/CodeBlockCell.swift
+++ TeachBot/CodeBlockCell.swift
@@ -82,23 +82,22 @@ class CodeBlockCell: UITableViewCell {
     }
     
     func configureColor() {
-        let imageName: String
         
         switch codeBlock! {
         case .Forward, .Backward:
             colorBackgroundView.backgroundColor = UIColor(red: 0.18, green: 0.8, blue: 0.443, alpha: 1.0)
-            imageName = "GreenTriangle"
+            arrowView.image = UIImage(named: "GreenTriangle")
         case .TurnLeft, .TurnRight:
             colorBackgroundView.backgroundColor = UIColor(red: 0.102, green: 0.737, blue: 0.612, alpha: 1.0)
-            imageName = "TurquoiseTriangle"
+            arrowView.image = UIImage(named: "TurquoiseTriangle")
         case .Stop:
             colorBackgroundView.backgroundColor = UIColor(red: 0.906, green: 0.298, blue: 0.235, alpha: 1.0)
-            imageName = "RedTriangle"
+            arrowView.image = UIImage(named: "RedTriangle")
         case .Wait(_):
             colorBackgroundView.backgroundColor = UIColor(red: 0.945, green: 0.769, blue: 0.059, alpha: 1.0)
-            imageName = "YellowTriangle"
+            arrowView.image = UIImage(named: "YellowTriangle")
         }
         
-        arrowView.image = UIImage(named: imageName)
+        
     }
 }
