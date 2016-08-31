95b7a270356a69dca3d51faead9fab2b09c81631
Peiliang Guo <edwardplguo@gmail.com>
2016-08-31 13:19:29 -0400

    Add insert functionality to MainViewController

--- TeachBot/MainViewController.swift
+++ TeachBot/MainViewController.swift
@@ -62,8 +62,15 @@ class MainViewController: UITableViewController {
     
     override func tableView(tableView: UITableView, editActionsForRowAtIndexPath indexPath: NSIndexPath) -> [UITableViewRowAction]? {
         
-        let insertAction = UITableViewRowAction(style: .Normal, title: "↓ Insert") { (_, _) in
-            NSLog("Insert")
+        let insertAction = UITableViewRowAction(style: .Normal, title: "↓ Insert") { [unowned self] (_, _) in
+            self.chooseCodeBlock() { (block) in
+                if block != nil {
+                    let newIndexPath = NSIndexPath(forRow: indexPath.row + 1, inSection: indexPath.section)
+                    dispatch_async(dispatch_get_main_queue(), {
+                        self.insertCodeBlock(block!, at: newIndexPath)
+                    });
+                }
+            }
         }
         
         let deleteAction = UITableViewRowAction(style: .Destructive, title: "Delete") { [unowned self] (_, path) in
@@ -75,7 +82,66 @@ class MainViewController: UITableViewController {
     }
     
     @IBAction func runCode(sender: UIButton) {
+        print(codeBlocks)
+    }
+    
+    func chooseCodeBlock(handler: (block: CodeBlock?) -> ()) {
+        let alertController = UIAlertController(title: "Choose a code block", message: nil, preferredStyle: .ActionSheet)
         
+        for block in CodeBlock.allEditableCodeBlocks {
+            
+            let action = UIAlertAction(title: block.description, style: .Default) { (_) in
+                handler(block: block)
+            }
+            
+            alertController.addAction(action)
+            
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel) { (_) in
+            handler(block: nil)
+        }
+        
+        alertController.addAction(cancelAction)
+        
+        presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    func insertCodeBlock(block: CodeBlock, at indexPath: NSIndexPath) {
+        switch block {
+        case .Wait(_):
+            let alertController = UIAlertController(title: "Wait Interval", message: "Time in seconds before next command to be executed, robot will remain running during a wait.", preferredStyle: .Alert)
+            
+            alertController.addTextFieldWithConfigurationHandler({ (field) in
+                field.placeholder = "Time Interval"
+                field.keyboardType = .DecimalPad
+            })
+            
+            let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+            alertController.addAction(cancelAction)
+            
+            let okAction = UIAlertAction(title: "OK", style: .Default) { [unowned self] (action) in
+                if let text = alertController.textFields![0].text {
+                    if let interval = Double(text) {
+                        if interval > 0.0 {
+                            let newBlock = CodeBlock.Wait(interval)
+                            
+                            self.codeBlocks.insert(newBlock, atIndex: indexPath.row)
+                            self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+                            
+                        }
+                    }
+                }
+                
+            }
+            alertController.addAction(okAction)
+            alertController.preferredAction = okAction
+            
+            presentViewController(alertController, animated: true, completion: nil)
+        default:
+            codeBlocks.insert(block, atIndex: indexPath.row)
+            tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        }
     }
 
 }
